980c7be6bea3f5d7224498b08612b65f2e6d1004
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2017-03-06 19:08:37 -0800

    Manoel is done for now

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import sys
 import time
+from random import randint
 import telepot
 
 def handle(msg):
@@ -15,12 +16,29 @@ def checkMsg(chat_id, msg, id):
     oop_strs = ("opp", "poo", "programacao orientada a objetos")
     nice_strs = ("python", "haskell", "erlang", "c", "lua", "brain", "lisp", "garnet")
     manoel_strs = ("manoel", "@lerax", "@lelerax")
+    hell_strs = ("satan", "diabo", "demonio", "inferno", "fogo", "hell")
+    bom_strs = ("bom", "que bom", "kibon", "massa", "funciona", "funcionando", "funcionou", "rodar", "rodou", "compilar", "compilou")
+    hue_strs = ("hue", "hu3", "br", "brasil", "brazil", "haha", "kkkk")
+
+    stk_card = "CAADAQADfwAD5G3CCTY2lvkcjTKUAg"
+    stk_monstro = "CAADAQAD3QAD5G3CCfzBqgbKrcVvAg"
+    stk_delicia1 = "CAADAgADhwAD12I0BVT0buDrX9dLAg"
+    stk_delicia2 = "CAADAgADcAAD12I0Bd-BteG_OrJCAg"
+    stk_aham = "CAADAgADhAAD12I0BVoDZEJxg6_KAg"
+    stk_fodase = "CAADAQADPQADarEOBvuO0Zx96BjxAg"
+    stk_queromorre = "CAADAQADRQADarEOBrMse4T-xV6SAg"
+    stk_satan = "CAADAQADtQADarEOBiaPApuCmIsjAg"
+    stk_hue = "CAADAQADxwAD5G3CCXbWeLF4Nd78Ag"
+    stk_kibon = "CAADAQADzwAD5G3CCdroYc38v2yHAg"
+    stk_flip = "CAADAQADyQAD5G3CCaRAcC_I79baAg"
     
-    res = ""
     m = msg.lower().split()
+    sticker = ""
+    res = ""
 
     if any(s in m for s in js_strs):
         res = "JS uma porra!\nJS e` tao cancer que comeca com J, de Javascript"
+        sticker = stk_fodase if randint(0,1) else stk_queromorre 
     elif any(s in m for s in cpp_strs):
         res = "C++ sucks! Stop using it." 
     elif any(s in m for s in oop_strs):
@@ -29,14 +47,27 @@ def checkMsg(chat_id, msg, id):
         res = "Continue to use that crap and you're out."
     elif any(s in m for s in nice_strs):
         res = "delicia"
-    elif any(s in m for s in manoel_strs) and "voltar" in msg.lower():
+        sticker = stk_delicia1 if randint(0,1) else stk_delicia2
+    elif "monstro" in msg.lower():
+        sticker = stk_monstro
+    elif any(s in m for s in hell_strs):
+        sticker = stk_satan
+    elif any(s in m for s in bom_strs):
+        bot.sendSticker(chat_id, stk_card)
+        sticker = stk_kibon if randint(0,1) else stk_flip
+    elif any(s in msg.lower() for s in hue_strs):
+        res = "AHUHAAUUhuahua"
+        sticker = stk_hue
+    elif any(s in msg.lower() for s in manoel_strs) and "voltar" in msg.lower():
         res = "to aqui carai"
-    elif any(s in m for s in manoel_strs):
+    elif any(s in msg.lower() for s in manoel_strs):
         res = "oi"
-    else:
-        return
 
-    bot.sendMessage(chat_id, res, reply_to_message_id=id)
+    if res:
+        bot.sendMessage(chat_id, res, reply_to_message_id=id)
+    
+    if sticker:
+        bot.sendSticker(chat_id, sticker)
 
 TOKEN = sys.argv[1]  # get token from command-line
 
