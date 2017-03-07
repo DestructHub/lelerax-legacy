031f423a44a7b4d547a2a9ff6057c445218df842
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2017-03-06 17:46:04 -0800

    Refactoring

--- bot.py
+++ bot.py
@@ -4,28 +4,39 @@ import telepot
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
-    #print(content_type, chat_type, chat_id)
 
     if content_type == 'text':
-        checkMsg(chat_id, msg['text'])
+        checkMsg(chat_id, msg['text'], msg['message_id'])
 
-def checkMsg(chat_id, msg):
+def checkMsg(chat_id, msg, id):
     js_strs = ("js", "javascript", "ecma script", "ecma", "ecma6")
-    cpp_strs = ("c++", "bjarne stroustrup", "c plus plus", "c with classes")
+    cpp_strs = ("c++", "bjarne stroustrup", "c plus plus", "c with classes", "cpp")
     java_strs = ("java", "java lambdas")
     oop_strs = ("opp", "poo", "programacao orientada a objetos")
-    nice_strs = ("python", "haskell", "erlang", "c")
+    nice_strs = ("python", "haskell", "erlang", "c", "lua", "brain", "lisp", "garnet")
+    manoel_strs = ("manoel", "@lerax", "@lelerax")
     
-    if any(s in msg.lower() for s in js_strs):
-        bot.sendMessage(chat_id, "JS sucks! Stop using it.")
-    elif any(s in msg.lower() for s in cpp_strs):
-        bot.sendMessage(chat_id, "C++ sucks! Stop using it.")     
-    elif any(s in msg.lower() for s in oop_strs):
-        bot.sendMessage(chat_id, "Such overrated concept, use functional paradigms!!!!!")
-    elif any(s in msg.lower() for s in java_strs):
-        bot.sendMessage(chat_id, "Continue to use that crap and you're out.")
-    elif any(s in msg.lower() for s in nice_strs):
-        bot.sendMessage(chat_id, "https://www.youtube.com/watch?v=ExsAwMTQwfI")
+    res = ""
+    m = msg.lower().split()
+
+    if any(s in m for s in js_strs):
+        res = "JS uma porra!\nJS e` tao cancer que comeca com J, de Javascript"
+    elif any(s in m for s in cpp_strs):
+        res = "C++ sucks! Stop using it." 
+    elif any(s in m for s in oop_strs):
+        res = "Such overrated concept, use functional paradigms!!!!!"
+    elif any(s in m for s in java_strs):
+        res = "Continue to use that crap and you're out."
+    elif any(s in m for s in nice_strs):
+        res = "delicia"
+    elif any(s in m for s in manoel_strs) and "voltar" in msg.lower():
+        res = "to aqui carai"
+    elif any(s in m for s in manoel_strs):
+        res = "oi"
+    else:
+        return
+
+    bot.sendMessage(chat_id, res, reply_to_message_id=id)
 
 TOKEN = sys.argv[1]  # get token from command-line
 
