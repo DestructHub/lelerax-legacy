b9950189b42d17af67f4e8f021e4446af8f1a86f
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2017-03-06 16:18:07 -0800

    First snippet of code

---
+++ bot.py
@@ -0,0 +1,26 @@
+import sys
+import time
+import telepot
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    #print(content_type, chat_type, chat_id)
+
+    if content_type == 'text':
+        checkMsg(chat_id, msg['text'])
+
+def checkMsg(chat_id, msg):
+    js_strs = ("js", "javascript")
+    if any(s in msg.lower() for s in js_strs):
+        bot.sendMessage(chat_id, "JS sucks!")
+
+TOKEN = sys.argv[1]  # get token from command-line
+
+bot = telepot.Bot(TOKEN)
+bot.message_loop(handle)
+print ('Listening ...')
+
+# Keep the program running.
+while 1:
+    time.sleep(10)
+
