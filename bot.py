111ae55e59b97163504c37af82e1c3eae9e1a183
Rafael C. Nunes <rafaelnunes737@hotmail.com>
2017-03-06 21:53:47 -0300

    Add even more response messages and use elif

--- bot.py
+++ bot.py
@@ -11,17 +11,21 @@ def handle(msg):
 
 def checkMsg(chat_id, msg):
     js_strs = ("js", "javascript", "ecma script", "ecma", "ecma6")
-    cpp_strs = ("c++", "Bjarne Stroustrup", "c plus plus", "c with classes")
-    oop_strs = ("OOP", "POO", "programação orientada a objetos")
+    cpp_strs = ("c++", "bjarne stroustrup", "c plus plus", "c with classes")
+    java_strs = ("java", "java lambdas")
+    oop_strs = ("opp", "poo", "programacao orientada a objetos")
+    nice_strs = ("python", "haskell", "erlang", "c")
     
     if any(s in msg.lower() for s in js_strs):
         bot.sendMessage(chat_id, "JS sucks! Stop using it.")
-    
-    if any(s in msg.lower() for s in cpp_strs):
-        bot.sendMessage(chat_id, "C++ sucks! Stop using it.")
-           
-    if any(s in msg.lower() for s in oop_strs):
+    elif any(s in msg.lower() for s in cpp_strs):
+        bot.sendMessage(chat_id, "C++ sucks! Stop using it.")     
+    elif any(s in msg.lower() for s in oop_strs):
         bot.sendMessage(chat_id, "Such overrated concept, use functional paradigms!!!!!")
+    elif any(s in msg.lower() for s in java_strs):
+        bot.sendMessage(chat_id, "Continue to use that crap and you're out.")
+    elif any(s in msg.lower() for s in nice_strs):
+        bot.sendMessage(chat_id, "https://www.youtube.com/watch?v=ExsAwMTQwfI")
 
 TOKEN = sys.argv[1]  # get token from command-line
 
