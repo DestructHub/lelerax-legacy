cdef68451239ba764e42b01d93cc4299ebc8e26f
Manoel Vilela <manoel_vilela@engineer.com>
2018-01-27 12:59:16 -0300

    Make better abstractions for bot.py
    Now this code is Data Oriented:
    
    + keywords
    + reactions
    + stickers
    
    You define this and all the things happens.

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from pprint import pprint
 from random import randint
 from datetime import datetime
 import telepot
+from random import choice
 
 UAPDA = -1001096359543  # UAPDA chat id, where lelerax born
 TOKEN = sys.argv[1]  # get token from command-line
@@ -14,6 +15,58 @@ bot = telepot.Bot(TOKEN)
 REACT_FACTOR = 10  # probability between of react: from 0 to 100 (max reaction)
 DEBUG = os.environ.get('DEBUG')
 ANSWER_THIS_SHIT = False
+
+# String -> Tuple[String]
+keywords = {
+    "js": ("js", "javascript", "ecmascript", "ecma", "ecma6"),
+    "cpp": ("c++", "bjarne stroustrup", "cancer++", "cpp"),
+    "java": ("java", "javismo", "jre", "jvm"),
+    "oop": ("opp", "poo", "smalltalk"),
+    "nice": ("python", "haskell", "erlang", "c", "lua", "brain", "lisp",
+              "rust", "cl"),
+    "manoel": ("manoel", "lelerax", "lerax", "ryukinix"),
+    "hell": ("satan", "diabo", "demonio", "inferno", "fogo", "hell", "suissa"),
+    "bom": ("bom", "kibon", "massa", "funciona", "funcionando",
+            "funcionou", "rodar", "rodou", "compilar", "compilou"),
+    "hue": ("hue", "hu3", "haha", "kkkk", "huashuash")
+
+}
+
+# String -> Tuple[String]
+reactions = {
+    "js": (":[ Jesus amado, JavaScript não... por favor.",
+           "Se JS fosse bom eu estava usando."),
+    "cpp": ( "C++... hmmm, pelo menos é melhor que Java e, obviamente, JS.",
+             "Parece que encontramos um.... MASOQUISTA"),
+    "java": ("Cara, Java? Aproveita e vai ler um livro de programação de verdade... coisa triste.",
+             "Gogo JAVA CHECKED EXCEPTIONS.",
+             "Pera aí, deixa eu comprar mais um pente de RAM rapidão aqui."),
+    "oop": ("Quantos objetos... AI MEU DEUS UMA CONDIÇÃO DE CORRIDA.",
+            "Meu Deus, quantos estados nesses objetos! D:"),
+    "nice": ("Excelente! Eu gostei.", "Aê, aprovado!", "Porra, muito bom, cara!",
+             "Ensina aí pra nóis", "Assim eu gosto", "Tem que ser assim mesmo!"),
+    "manoel": ("To aqui, carai!", "Oi"),
+    "hell": ("Não profira nome de demônio na casa sagrada do resto da APDA que presta.",
+             "Rapaz que parada mais cancer."),
+    "hue": ("kkkkkkkk", "caramba kkk", "carai mano kkkk", "porra kkkk", "fala serio kkkk",
+            "toma kkkkk"),
+}
+
+
+# String -> String
+stickers = {
+    "card": "CAADAQADfwAD5G3CCTY2lvkcjTKUAg",
+    "monstro": "CAADAQAD3QAD5G3CCfzBqgbKrcVvAg",
+    "delicia1": "CAADAgADhwAD12I0BVT0buDrX9dLAg",
+    "delicia2": "CAADAgADcAAD12I0Bd-BteG_OrJCAg",
+    "fodase": "CAADAQADPQADarEOBvuO0Zx96BjxAg",
+    "queromorre": "CAADAQADRQADarEOBrMse4T-xV6SAg",
+    "satan": "CAADAQADtQADarEOBiaPApuCmIsjAg",
+    "hue": "CAADAQADxwAD5G3CCXbWeLF4Nd78Ag",
+    "kibon": "CAADAQADzwAD5G3CCdroYc38v2yHAg",
+    "flip": "CAADAQADyQAD5G3CCaRAcC_I79baAg"
+}
+
 
 
 def react(x=REACT_FACTOR):
@@ -43,29 +96,6 @@ def bot_engine(chat_id, msg, message_id):
 
 def bot_answers(chat_id, msg, message_id):
     global ANSWER_THIS_SHIT
-    js_strs = ("js", "javascript", "ecma script", "ecma", "ecma6")
-    cpp_strs = ("c++", "bjarne stroustrup", "c plus plus", "c with classes",
-                "cpp")
-    java_strs = ("java", "java lambdas")
-    oop_strs = ("opp", "poo", "programacao orientada a objetos")
-    nice_strs = ("python", "haskell", "erlang", "c", "lua", "brain", "lisp",
-                 "garnet")
-    manoel_strs = ("manoel", "@lerax", "@lelerax", "lerax")
-    hell_strs = ("satan", "diabo", "demonio", "inferno", "fogo", "hell")
-    bom_strs = ("bom", "que bom", "kibon", "massa", "funciona", "funcionando",
-                "funcionou", "rodar", "rodou", "compilar", "compilou")
-    hue_strs = ("hue", "hu3", "haha", "kkkk")
-
-    stk_card = "CAADAQADfwAD5G3CCTY2lvkcjTKUAg"
-    stk_monstro = "CAADAQAD3QAD5G3CCfzBqgbKrcVvAg"
-    stk_delicia1 = "CAADAgADhwAD12I0BVT0buDrX9dLAg"
-    stk_delicia2 = "CAADAgADcAAD12I0Bd-BteG_OrJCAg"
-    stk_fodase = "CAADAQADPQADarEOBvuO0Zx96BjxAg"
-    stk_queromorre = "CAADAQADRQADarEOBrMse4T-xV6SAg"
-    stk_satan = "CAADAQADtQADarEOBiaPApuCmIsjAg"
-    stk_hue = "CAADAQADxwAD5G3CCXbWeLF4Nd78Ag"
-    stk_kibon = "CAADAQADzwAD5G3CCdroYc38v2yHAg"
-    stk_flip = "CAADAQADyQAD5G3CCaRAcC_I79baAg"
 
     msg_norm = msg.lower()
     m = msg_norm.split()
@@ -76,41 +106,23 @@ def bot_answers(chat_id, msg, message_id):
     if 'lelerax' in msg_norm:
         ANSWER_THIS_SHIT = True
 
-    if react(20) and any(s in m for s in js_strs):
-        res = ":[ Jesus amado, JavaScript não... por favor."
-        sticker = stk_fodase if react(10) else stk_queromorre
-    elif react(20) and any(s in m for s in cpp_strs):
-        res = "C++... hmmm, pelo menos é melhor que Java e, obviamente, JS."
-    elif react(20) and any(s in m for s in oop_strs):
-        res = "Quantos objetos... AI MEU DEUS UMA CONDIÇÃO DE CORRIDA."
-    elif react(20) and any(s in m for s in java_strs):
-        res = "Cara, Java? Aproveita e vai ler um livro de programação de verdade... coisa triste."
-    elif react(50) and any(s in m for s in nice_strs):
-        res = "Excelente! Eu gostei."
-        sticker = stk_delicia1 if react() else stk_delicia2
-    elif "monstro" in msg_norm:
-        sticker = stk_monstro
-    elif any(s in m for s in hell_strs):
-        sticker = stk_satan
-    elif any(s in m for s in bom_strs):
-        if stk_bool:
-            bot.sendSticker(chat_id, stk_card)
-        sticker = stk_kibon if react() else stk_flip
-    elif react(5) and any(s in msg_norm for s in hue_strs):
-        res = "kkkkkkkk"
-        sticker = stk_hue
-    elif react(30) and "suissa" in msg_norm:
-        res = "Não profira nome de demônio na casa sagrada do resto da APDA que presta."
-        sticker = stk_hue
-    elif react(50) and any(s in msg_norm for s in manoel_strs) and "voltar" in msg_norm:
-        res = "To aqui, carai!"
-    elif react(50) and any(s in msg_norm for s in manoel_strs):
-        res = "Oi"
+    for category, keys in keywords.items():
+        if react(30) and category in reactions and any(s in m for s in keys):
+            res = choice(reactions[category])
+
+    if "monstro" in msg_norm:
+        sticker = stickers["monstro"]
+    elif any(s in m for s in keywords["hell"]):
+        sticker = stickers["satan"]
+    elif any(s in m for s in keywords["bom"]):
+        if stickers["bool"]:
+            bot.sendSticker(chat_id, stickers["card"])
+        sticker = stickers["kibon"] if react() else stickers["flip"]
 
     if res:
         bot.sendMessage(chat_id, res, reply_to_message_id=message_id)
 
-    if sticker and stk_bool:
+    if sticker and stickers["bool"]:
         bot.sendSticker(chat_id, sticker)
 
     ANSWER_THIS_SHIT = False
